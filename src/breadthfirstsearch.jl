052a978377181b3e1e12d1b467d0f7baadbeea46
Ming-Chiang Chang <mc2663@cornell.edu>
2021-05-27 16:31:49 -0400

    add readme and test file

--- src/breadthfirstsearch.jl
+++ src/breadthfirstsearch.jl
@@ -4,8 +4,9 @@ struct BreadthFirstSearch{T, PH<:AbstractVector{<:Phase{T}},
 			PT<:AbstractVector{<:SmoothPattern{T}}, AS<:AbstractVector{Bool},
 			XT<:AbstractVecOrMat, RT<:AbstractVecOrMat, AT<:AbstractVecOrMat,
 			NS<:Real, PV<:AbstractVector{T}}
-	phases::PH
-	patterns::PT
+			# Point of doing it like this? (template-like)
+	current_phases::PH
+	next_phases::PH
 	isactive::AS
 
 	x::XT
@@ -17,9 +18,17 @@ struct BreadthFirstSearch{T, PH<:AbstractVector{<:Phase{T}},
 	prior_mean::PV # prior mean for a, α, σ
 	prior_std::PV # prior std for a, α, σ
 
+	# Parameter for stopping
+	# 1. Low residual (good stop)
+	# 2. To many extra peaks (bad stop)
+	#    Condition: norm(max.(fitted-data)./data) ?
+	#    Or is there anything better than 2 norm
+	res_thresh::Real
+    extra_norm::Real
 end
 
 const BFS = BreadthFirstSearch
-function BFS()
+function BFS(phase::AbstractVector{<:Phase}, x::AbstractVector,
+	         y::AbstractVector)
     return
 end
